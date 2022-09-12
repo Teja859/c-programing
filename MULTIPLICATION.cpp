70a56ca119ec7454fe1c70988c58f8ae1e1876d1
Teja859 <112815582+Teja859@users.noreply.github.com>
2022-09-12 14:27:25 +0530

    Add files via upload

---
+++ MULTIPLICATION.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    double n, m, product;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+    product = a * 
+    printf("Product = %.2lf", product);
+    return 0;
+}
